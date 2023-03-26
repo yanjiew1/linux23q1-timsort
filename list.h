9b9895d50997cf0a02c954418a15137e7d6db96e
Yan-Jie Wang <yanjiewtw@gmail.com>
2023-03-26 19:09:43 +0800

    Add container_of macro

--- list.h
+++ list.h
@@ -26,6 +26,23 @@ struct hlist_node {
 	struct hlist_node *next, **pprev;
 };
 
+#ifndef offsetof
+#define offsetof(TYPE, MEMBER) ((size_t) &((TYPE *)0)->MEMBER)
+#endif
+
+#ifndef container_of
+/**
+ * container_of - cast a member of a structure out to the containing structure
+ * @ptr:	the pointer to the member.
+ * @type:	the type of the container struct this is embedded in.
+ * @member:	the name of the member within the struct.
+ *
+ */
+#define container_of(ptr, type, member) ({			\
+	const typeof(((type *)0)->member) * __mptr = (ptr);	\
+	(type *)((char *)__mptr - offsetof(type, member)); })
+#endif
+
 #define LIST_HEAD_INIT(name) { &(name), &(name) }
 
 #define LIST_HEAD(name) \
