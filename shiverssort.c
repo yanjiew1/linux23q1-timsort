1304c0c1de9c6481a03362e2c45e9946748cfc14
Yan-Jie Wang <yanjiewtw@gmail.com>
2023-03-26 18:01:04 +0800

    Add shiverssort

---
+++ shiverssort.c
@@ -0,0 +1,151 @@
+#include "list.h"
+#include "list_sort.h"
+
+#include <stdint.h>
+
+#ifndef likely
+#define likely(x) __builtin_expect(!!(x), 1)
+#endif
+#ifndef unlikely
+#define unlikely(x) __builtin_expect(!!(x), 0)
+#endif
+
+#define MAX_MERGE_PENDING 85
+
+struct run {
+	struct list_head *list;
+	size_t len;
+};
+
+static struct list_head *merge(void *priv, list_cmp_func_t cmp,
+			       struct list_head *a, struct list_head *b)
+{
+	struct list_head *head, **tail = &head, **node;
+
+	do {
+		/* if equal, take 'a' -- important for sort stability */
+		node = cmp(priv, a, b) <= 0 ? &a : &b;
+		*tail = *node;
+		tail = &(*node)->next;
+	} while ((*node = (*node)->next));
+	*tail = (struct list_head *)((uintptr_t)a | (uintptr_t)b);
+	return head;
+}
+
+static void build_prev_link(struct list_head *head, struct list_head *tail,
+			    struct list_head *list)
+{
+	tail->next = list;
+	do {
+		list->prev = tail;
+		tail = list;
+		list = list->next;
+	} while (list);
+
+	/* The final links to make a circular doubly-linked list */
+	tail->next = head;
+	head->prev = tail;
+}
+
+static void merge_final(void *priv, list_cmp_func_t cmp, struct list_head *head,
+			struct list_head *a, struct list_head *b)
+{
+	struct list_head *tail = head, **node;
+
+	do {
+		/* if equal, take 'a' -- important for sort stability */
+		node = cmp(priv, a, b) <= 0 ? &a : &b;
+		tail->next = *node;
+		(*node)->prev = tail;
+		tail = *node;
+	} while ((*node = (*node)->next));
+	b = (struct list_head *)((uintptr_t)a | (uintptr_t)b);
+
+	/* Finish linking remainder of list b on to tail */
+	build_prev_link(head, tail, b);
+}
+
+static struct list_head *find_run(void *priv, struct list_head *list,
+				  size_t *len, list_cmp_func_t cmp)
+{
+	*len = 1;
+	struct list_head *next = list->next;
+
+	if (unlikely(next == NULL))
+		return NULL;
+
+	if (cmp(priv, list, next) > 0) {
+		/* decending run, also reverse the list */
+		struct list_head *prev = NULL;
+		do {
+			(*len)++;
+			list->next = prev;
+			prev = list;
+			list = next;
+			next = list->next;
+		} while (next && cmp(priv, list, next) > 0);
+	} else {
+		do {
+			(*len)++;
+			list = next;
+			next = list->next;
+		} while (next && cmp(priv, list, next) <= 0);
+	}
+
+	list->next = NULL;
+	return next;
+}
+
+static void merge_at(void *priv, list_cmp_func_t cmp, struct run *at)
+{
+	at[0].list = merge(priv, cmp, at[0].list, at[1].list);
+	at[0].len += at[1].len;
+}
+
+static struct run *merge_collapse(void *priv, list_cmp_func_t cmp,
+				  struct run *stk, struct run *tp)
+{
+	while ((tp - stk + 1) >= 3) {
+		size_t x = tp[-1].len | tp[0].len;
+		if (x <= (tp[-2].len & ~x))
+			break;
+		merge_at(priv, cmp, &tp[-2]);
+		tp[-1] = tp[0];
+		tp--;
+	}
+
+	return tp;
+}
+
+void shiverssort(void *priv, struct list_head *head, list_cmp_func_t cmp)
+{
+	struct list_head *list = head->next;
+	struct run stk[MAX_MERGE_PENDING], *tp = stk - 1;
+
+	if (head == head->prev)
+		return;
+
+	/* Convert to a null-terminated singly-linked list. */
+	head->prev->next = NULL;
+
+	do {
+		tp++;
+		/* Find next run */
+		tp->list = list;
+		list = find_run(priv, list, &tp->len, cmp);
+		tp = merge_collapse(priv, cmp, stk, tp);
+	} while (list);
+
+	/* End of input; merge together all the runs. */
+	while (tp > stk + 1) {
+		tp[-1].list = merge(priv, cmp, tp[-1].list, tp[0].list);
+		tp--;
+	}
+
+	/* The final merge; rebuild prev links */
+	if (tp > stk) {
+		merge_final(priv, cmp, head, stk[0].list, stk[1].list);
+	} else {
+		build_prev_link(head, head, stk->list);
+	}
+}
