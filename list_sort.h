1b0081f7d7210ca4da78c804351e9f9ab452a384
Yan-Jie Wang <yanjiewtw@gmail.com>
2023-03-26 20:06:35 +0800

    Add newly add sorting algo to header

--- list_sort.h
+++ list_sort.h
@@ -7,3 +7,5 @@ typedef int (*list_cmp_func_t)(void *,
 		const struct list_head *, const struct list_head *);
 
 void list_sort(void *priv, struct list_head *head, list_cmp_func_t cmp);
+void shiverssort(void *priv, struct list_head *head, list_cmp_func_t cmp);
+void timsort(void *priv, struct list_head *head, list_cmp_func_t cmp);
